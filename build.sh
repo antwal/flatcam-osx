a368922b42c23e97b02bcb3eb503b5e935c56a27
Walter Antolini <antwal@gmail.com>
2020-12-03 22:54:55 +0100

    Try fix error on all OSX versions

--- build.sh
+++ build.sh
@@ -19,21 +19,31 @@ cd FlatCAM_beta_8.993_sources
 virtualenv env
 # Activate the virtual environment
 source env/bin/activate
+# Upgrade PIP
+python -m pip install --upgrade pip
 # Install all Python dependencies in the virtual environment
 pip3 install numpy matplotlib rtree scipy shapely simplejson lxml rasterio ezdxf svg.path freetype-py fontTools ortools vispy PyOpenGL PyQT5
+# Test
+ls -al ./env/bin
 # Get out of the virtual environment
 deactivate
 
-cd ..
+# cd ..
 
 # Copy a script to execute FlatCAM
-cp ./FlatCam ./FlatCAM_beta_8.993_sources/
+echo "Copy Execute Script"
+cp ../FlatCam ./FlatCAM_beta_8.993_sources/
 
 # Copy the AppleScript
-cp ./FlatCAM.scpt ./FlatCAM_beta_8.993_sources/
+echo "Copy Compile Script"
+cp ../FlatCAM.scpt ./FlatCAM_beta_8.993_sources/
 
 # Compile the AppleScript into an application
-osacompile -o ./FlatCAM-${OS_NAME}-${OS_VERSION}.app ./FlatCAM_beta_8.993_sources/FlatCAM.scpt
+echo "Build..."
+osacompile -o ../FlatCAM-${OS_NAME}-${OS_VERSION}.app ./FlatCAM.scpt
 
+# Test End
+echo "Test end"
+ls -al
 
-
+ls -al ../
